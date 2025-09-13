9591f845afe1f13e579923b05a3600f8a5790150
coderabhay332 <akgabhay11@gmail.com>
2025-09-13 18:16:20 +0530

    fastagseva fe

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  rules: {
+    // Disable conflicting rules
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
